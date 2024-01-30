ecbca2b449ac43dcb760bd8857e6b739ccf7bbd8
mabao0226 <mabao0226@gmail.com>
2024-01-30 20:02:06 +0800

    update test3.js

---
+++ src/test3.js
@@ -0,0 +1,14 @@
+const main = async () => {
+    console.log("hello world main1");
+}
+main()
+
+const main2 = () => {
+    console.log("hello world main2");
+}
+main2()
+
+const main3 = function () {
+    console.log("hello world main3");
+}
+main3()
